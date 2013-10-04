1b8bc7de7f50e57a4bdb62f1f6334698f75e9c0d
Daniel Dutton <Dannybdutton@Gmail.com>
2013-10-04 14:05:56 +0100

    Skeleton test cases added

---
+++ src/unitTests/FahrenheitToCelsiusTest.java
@@ -0,0 +1,19 @@
+package unitTests;
+
+
+public class FahrenheitToCelsiusTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testConvert() throws Exception {
+
+    }
+}

441de07a9b769bf0c2a41da0df562abaaeeb5ec7
Daniel Dutton <Dannybdutton@Gmail.com>
2013-10-04 13:52:46 +0100

    Project structure refactor

--- src/model/TemperatureUnicode.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class TemperatureUnicode {
-    public static final char DEGREE_F = '\u2109';
-	public static final char DEGREE_C = '\u2103';
-	public static final char DEGREE_K = '\u212A';
-}
